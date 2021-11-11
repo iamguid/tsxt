ab458bf6bf04dfd6e3c234576ad6db80cd2d7ead
Vladislav Lebedev <iam.guid@gmail.com>
2021-11-11 23:25:26 +0500

    Add ln handler

--- src/visitor.ts
+++ src/visitor.ts
@@ -1,42 +1,11 @@
 import { Node, NodePath, Visitor } from "@babel/core"
-import { BinaryExpression, binaryExpression, Expression, expressionStatement, ExpressionStatement, isArrowFunctionExpression, isExpression, isJSXEmptyExpression, isJSXExpressionContainer, JSXElement, JSXExpressionContainer, ReturnStatement, returnStatement, stringLiteral } from "@babel/types";
+import { BinaryExpression, binaryExpression, Expression, expressionStatement, ExpressionStatement, isArrowFunctionExpression, isExpression, isJSXElement, isJSXEmptyExpression, isJSXExpressionContainer, JSXElement, jSXExpressionContainer, JSXExpressionContainer, ReturnStatement, returnStatement, stringLiteral } from "@babel/types";
 import { VisitorState } from ".";
 import { UnexpectedType } from "./errors";
 import { getJSXElementName } from "./helpers";
 import { TagName } from "./tags";
 
 export type Handler<TNode extends Node> = (path: NodePath<TNode>, state: VisitorState) => void;
-
-// const generateTemplate = (path: NodePath<JSXElement>, state: VisitorState) => {
-//     const nodesStack: Node[] = [...path.node.children];
-//     const resultExpressions: Node[] = [];
-
-//     while (nodesStack.length > 0) {
-//         const node = nodesStack.shift()!;
-
-//         console.warn(node.type);
-
-//         if (isJSXText(node)) {
-//             continue;
-//         } else if (isTemplateLiteral(node)) {
-//             resultExpressions.push(binaryExpression('+', node, stringLiteral("\n")));
-//         } else if (isJSXExpressionContainer(node)) {
-//             nodesStack.push(node.expression);
-//         } else if (isExpressionStatement(node)) {
-//             nodesStack.push(node.expression)
-//         } else if (isCallExpression(node)) {
-//             nodesStack.push(...node.arguments)
-//         } else if (isArrowFunctionExpression(node)) {
-//             nodesStack.push(node.body)
-//         } else if (isBlockStatement(node)) {
-//             nodesStack.push(...node.body)
-//         } else {
-//             continue;
-//         }
-//     }
-
-//     return resultExpressions; 
-// }
 
 const handleJSXTemplElement = (path: NodePath<JSXElement>, state: VisitorState) => {
     const resultNodes: Expression[] = (path.node.children as JSXExpressionContainer[])
@@ -60,6 +29,7 @@ const handleJSXTemplElement = (path: NodePath<JSXElement>, state: VisitorState) 
 }
 
 const handleJSXLnElement = (path: NodePath<JSXElement>, state: VisitorState) => {
+    path.replaceWith(jSXExpressionContainer(stringLiteral('')))
 }
 
 const handleJSXIndentElement = (path: NodePath<JSXElement>, state: VisitorState) => {
@@ -68,9 +38,7 @@ const handleJSXIndentElement = (path: NodePath<JSXElement>, state: VisitorState)
 const handleJSXCbElement = (path: NodePath<JSXElement>, state: VisitorState) => {
 }
 
-export const handlers: Partial<Record<Node['type'], Handler<Node>>> = {};
-
-export const jsxHandlers: Record<TagName, Handler<JSXElement>> = {
+export const handlers: Record<TagName, Handler<JSXElement>> = {
     'templ': handleJSXTemplElement,
     'indent': handleJSXIndentElement,
     'ln': handleJSXLnElement,
@@ -78,26 +46,30 @@ export const jsxHandlers: Record<TagName, Handler<JSXElement>> = {
 };
 
 const visitor: Visitor<VisitorState> = {
-    JSXElement: (path: NodePath<JSXElement>, state: VisitorState) => {
-        const name = getJSXElementName(path) as TagName;
+    JSXElement: {
+        enter: (path: NodePath<JSXElement>, state: VisitorState) => {
+            const name = getJSXElementName(path) as TagName;
 
-        try {
-            if (!(name in jsxHandlers)) {
-                return;
+            if (name !== 'templ' && name in handlers) {
+                handlers[name](path as NodePath<JSXElement>, state);
             }
+        },
+        exit: (path: NodePath<JSXElement>, state: VisitorState) => {
+            const name = getJSXElementName(path) as TagName;
 
-            jsxHandlers[name](path, state);
-            
-            return;
-        } catch (e) {
-            if (e instanceof TypeError) {
-                throw new UnexpectedType(path as NodePath<Node>, e);
+            if (name === 'templ') {
+                try {
+                    handleJSXTemplElement(path as NodePath<JSXElement>, state);
+                } catch (e) {
+                    if (e instanceof TypeError) {
+                        throw new UnexpectedType(path as NodePath<Node>, e);
+                    }
+
+                    throw e;
+                }
             }
-
-            throw e;
         }
     }
-
 }
 
 export default visitor;
---
+++ tests/ln.template.tsx
@@ -0,0 +1,15 @@
+export default function generate(ctx: any) {
+    return (
+        <templ>
+            {'test1'}
+            <ln />
+            {'test2'}
+            <ln />
+            <ln />
+            {'test3'}
+        </templ>
+    )
+
+}
+
+console.log({})
